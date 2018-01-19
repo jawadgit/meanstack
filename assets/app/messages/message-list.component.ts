737336e658ba523c371d9680aa5a58d4d4a1d4bb
jawadnisar <jawad@ivyconnect.pk>
2018-01-19 23:22:13 +0500

    messages crud frontend / backend done

--- assets/app/messages/message-list.component.ts
+++ assets/app/messages/message-list.component.ts
@@ -14,6 +14,11 @@ export class MessageListComponent implements OnInit{
     constructor(private messageService: MessageService){}
 
     ngOnInit(){
-        this.messages = this.messageService.getMessage();
+        this.messageService.getMessages()
+            .subscribe(
+                (messages: Message[]) => {
+                    this.messages = messages;
+                }
+            );
     }
 }
